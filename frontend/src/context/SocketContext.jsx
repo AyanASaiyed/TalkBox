44cefdaf7ce858574931e7f4b07e46347b81d13e
Ayan Saiyed <145615216+AyanASaiyed@users.noreply.github.com>
2024-09-06 21:14:10 -0400

    updated socket url

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -15,7 +15,7 @@ export const SocketContextProvider = ({ children }) => {
 
   useEffect(() => {
     if (authUser) {
-      const socket = io("http://localhost:5000", {
+      const socket = io("https://talkbox-m6vo.onrender.com", {
         query: {
           userId: authUser._id,
         },
